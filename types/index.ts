b39325ca04605af3346a40fafccaa3f0c9b2c26f
Priyanshu Bhandari <technopriyu@gmail.com>
2023-06-09 17:57:51 +0530

    added navbar

--- types/index.ts
+++ types/index.ts
@@ -3,6 +3,8 @@ import { MouseEventHandler } from "react";
 export interface CustomButtonProps {
   title: string | string[];
   containerStyles?: string;
+  btnType?: "button" | "submit";
   handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
 // interface specifies how a structure should look like, what variables and values it should have
+// https://stackoverflow.com/questions/37233735/interfaces-vs-types-in-typescript
